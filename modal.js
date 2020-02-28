8cc35edc4977a73697185451b5310e4f5216abe0
mattproctorleake@gmail.com <mattproctorleake@gmail.com>
2020-02-28 12:28:13 +0000

    added TABS

--- modal.js
+++ modal.js
@@ -1,3 +1,37 @@
+// this is for TABS
+
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+// use Array.from if you have a NodeList and want to use .find 
+//this creates an array
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(e) {
+    //this hides all tab panels
+    tabPanels.forEach(panel => {
+        panel.hidden = true;
+    });
+    // mark all tabs as unselected
+tabButtons.forEach(tab => {
+  tab.setAttribute('aria-selected', false);
+});
+//mark clicked tab as selected
+e.currentTarget.setAttribute('aria-selected', true);
+
+const { id } = e.currentTarget;
+
+//METHOD 1
+// const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+// console.log(tabPanel.hidden);
+// tabPanel.hidden = false;
+
+//METHOD 2
+const tabPanel = tabPanels.find(panel => panel.getAttribute
+    ('aria-labelledby') === id);
+    tabPanel.hidden = false;
+}
+
+
 //this is for scroll box 
 const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
@@ -19,6 +53,9 @@ function obCallback(payload) {
   
   ob.observe(terms.lastElementChild);
 
+
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
 
 // this is for 4 x modals
 const cardButtons = document.querySelectorAll('.card button');
